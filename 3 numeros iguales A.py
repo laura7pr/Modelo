c77b1fb476bf9c5bc5716e6b2f61918660ff3bd2
laura7pr <33682859+laura7pr@users.noreply.github.com>
2017-11-17 11:04:18 +0100

    problemas día 17

---
+++ 3 numeros iguales A.py
@@ -0,0 +1,17 @@
+n1=int(input("numero 1"))
+
+n2=int(input("numero 2"))
+
+n3=int(input("numero 3"))
+
+if (n1== n2== n3):
+
+  print("iguales")
+
+elif (n1==n2!=n3)or(n1!=n2==n3)or(n1==n3!=n2):
+
+  print("2 iguales")
+
+elif (n1!=n2!=n3):
+
+  print("distintos")
---
+++ 3 numeros iguales B.py
@@ -0,0 +1,17 @@
+n1=int(input("numero 1"))
+
+n2=int(input("numero 2"))
+
+n3=int(input("numero 3"))
+
+if (n1== n2== n3):
+
+  print("iguales")
+
+elif (n1==n2)or(n2==n3)or(n1==n3):
+
+  print("2 iguales")
+
+else:
+
+  print("distintos")
