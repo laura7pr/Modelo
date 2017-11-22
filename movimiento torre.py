ae56e8cfefec67828c834aaf27df952e5a21e8c1
laura7pr <33682859+laura7pr@users.noreply.github.com>
2017-11-22 09:17:46 +0100

    Programa día 22

---
+++ movimiento torre.py
@@ -0,0 +1,19 @@
+﻿columna= int(input("columna 1"))
+
+fila= int(input("fila 1"))
+
+columna2= int(input("columna 2"))
+
+fila2= int(input("fila2"))
+
+if columna ==columna2:
+
+  print("Sí")
+
+elif fila==fila2:
+
+  print("Sí")
+
+else:
+
+  print ("No")
