12e54e3d2dfaa4107279b4098c7b0106c323b9a1
laura7pr <33682859+laura7pr@users.noreply.github.com>
2017-11-24 10:57:43 +0100

    Problemas ajedrez día 24-11

---
+++ casillas blancas y negras.py
@@ -0,0 +1,11 @@
+columna= int(input("columna"))
+
+fila= int(input("fila"))
+
+if (columna+fila)%2==0:
+
+  print("Negra")
+
+else:
+
+  print("Blanca")
---
+++ casillas iguales o distintas .py
@@ -0,0 +1,27 @@
+columna= int(input("columna"))
+
+fila= int(input("fila"))
+
+columna2= int(input("columna2"))
+
+fila2= int(input("fila2"))
+
+if (columna+fila)%2==0 and (columna2+fila2)%2==0:
+
+  print ("Igual")
+
+elif (columna+fila)%2!=0 and (columna2+fila2)%2!=0:
+
+  print ("Igual")
+#if (columna+fila)%2==0 and (columna2+fila2)%2!=0:
+
+#  print ("Distinta")
+
+#elif (columna+fila)%2!=0 and (columna2+fila2)%2==0:
+
+#  print ("Distinta")
+
+
+else:
+
+  print ("Distinto")
