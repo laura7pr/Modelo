9c1fb2ff913ddfccd42775367e006de4115b248b
laura7pr <33682859+laura7pr@users.noreply.github.com>
2017-12-15 10:38:42 +0100

    Programa día 12 de diciembre

---
+++ Movimiento rey.py
@@ -0,0 +1,34 @@
+columna= int(input("columna 1"))
+
+fila= int(input("fila 1"))
+
+columna2= int(input("columna 2"))
+
+fila2= int(input("fila2"))
+
+if (columna2==columna+1)and (columna2==columna-1):
+
+  print("si se puede mover")
+
+elif (fila2==fila+1)and(fila2==fila-1):
+
+  print("si se puede")
+
+elif (fila2!=fila+1) and (fila2!=fila-1):
+
+  print("no se puede")
+
+elif(columna2!=columna+1)and(columna2!=columna-1):
+
+  print("no se puede")
+
+elif (columna+1)and(fila+1):
+
+  print("si se puede")
+
+elif (columna-1)and(fila-1):
+
+  print("si se puede")
+
+else:
+  print("no se puede")
