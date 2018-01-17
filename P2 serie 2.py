79990aaf81d7b1940d294f55e6c0b5f66b1fe5ac
laura7pr <33682859+laura7pr@users.noreply.github.com>
2018-01-17 09:02:09 +0100

    trabajo día 17

---
+++ P2 serie 2.py
@@ -0,0 +1,15 @@
+﻿n1= int(input("Número 1"))
+
+n2= int(input("Número 2"))
+
+if (n1>n2):
+
+  for t in range (n1,n2-1, -1):
+
+    print (t)
+
+else:
+ 
+ for t in range(n1, n2+1, 1):
+
+    print(t)
