931e61cc013e9f3b7f8465f0b20c9872ab557f5f
laura7pr <33682859+laura7pr@users.noreply.github.com>
2018-02-02 10:54:18 +0100

    trabajo día 02/02

---
+++ P7 número de ceros.py
@@ -0,0 +1,19 @@
+﻿n=int(input("¿cuantos numeros quieres decirme?"))
+
+j=0
+
+for i in range(1,n+1):
+
+  cosa=int(input("dime el " + str(i) + "º numero"  ))
+
+  if (cosa==0):
+
+    j=j+1
+
+    print ("+ un 0")
+
+  else:
+
+    print("no es 0")
+
+print("hay",j, "ceros")
