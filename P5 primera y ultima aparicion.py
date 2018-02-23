b68c7420b26d9b9551a87789487366402b6da883
laura7pr <33682859+laura7pr@users.noreply.github.com>
2018-02-23 10:32:13 +0100

    trabajo dia 23

---
+++ P5 primera y ultima aparicion.py
@@ -0,0 +1,11 @@
+﻿frase=str(input("dime una frase:"))
+
+palabra=str(input("¿que letra buscas?"))
+
+primera =frase .find(palabra)
+
+print(primera+1)
+
+segunda =frase .rfind(palabra)
+
+print(segunda+1)
