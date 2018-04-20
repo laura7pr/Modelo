a745e0a59eb432470264b4eaa2cbbc13f0e2c7c1
laura7pr <33682859+laura7pr@users.noreply.github.com>
2018-04-20 10:59:53 +0200

    trabajo dia 20

---
+++ P6 media de una secuencia.py
@@ -0,0 +1,13 @@
+cantidad = 0
+suma = 0
+media = 1
+while True:
+  cantidad += 1
+  n = int(input('Dime un número:'))
+  suma = suma + n
+  if n == 0:
+    break
+print('El número de números introducidos es: '+ str(cantidad-1))
+print('La suma de todos los números introducidos son: '+str(suma))
+media = suma/(cantidad-1)
+print('La media de la secuencia es: ' + str(media))
